81e1e6aafaf5d52895a499fea6f0025958483c01
bryan mcwatters <bmcwatters@comcast.net>
2021-12-13 13:07:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myGames = new List<string>() { "Space Invaders", "Pac-Man", "Pit Fall", "Mario" };
+
+            var sorted = myGames.OrderBy(x => x);
+
+            myGames.Sort();
+
+            foreach(var game in sorted)
+            {
+
+                Console.WriteLine(game);
+            }
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
